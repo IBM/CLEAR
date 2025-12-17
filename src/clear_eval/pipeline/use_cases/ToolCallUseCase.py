269393a33e54bc5a9032105c0aef79040df4fbb6
Koren Lazar <koren.lazar@ibm.com>
2025-12-17 13:18:38 +0200

    First implementation of SPARC in CLEAR. Updated dependencies accordingly.

--- src/clear_eval/pipeline/use_cases/ToolCallUseCase.py
+++ src/clear_eval/pipeline/use_cases/ToolCallUseCase.py
@@ -1,14 +1,19 @@
 import json
-import os
+from importlib.resources import files
 from typing import Tuple, Any, List, Dict
+import numpy as np
 import pandas as pd
 from clear_eval.pipeline.use_cases.EvalUseCase import EvalUseCase
 from clear_eval.pipeline.constants import EVALUATION_TEXT_COL, SCORE_COL
+from altk.pre_tool.sparc import SPARCReflectionComponent
+from altk.core.toolkit import AgentPhase, ComponentConfig
+from altk.pre_tool.core import SPARCExecutionMode, SPARCReflectionRunInput, Track, SPARCReflectionResult
+from altk.core.llm import get_llm, BaseLLMClient
 
 import logging
 
-from pipeline.config_loader import load_config
-from pipeline.llm_chat_utils import get_chat_llm
+from clear_eval.pipeline.config_loader import load_config
+from clear_eval.pipeline.llm_chat_utils import get_chat_llm
 
 logger = logging.getLogger(__name__)
 
@@ -25,17 +30,11 @@ class ToolCallEvalUseCase(EvalUseCase):
         df[EVALUATION_TEXT_COL] = ""
         df[score_col] = pd.NA  # Use Pandas NA for missing scores
 
-        examples = []
-        for i, row in df.iterrows():
-            examples.append({self.ID_COL:row[self.ID_COL],
-                             self.CONTEXT_COL: json.loads(row[self.CONTEXT_COL]),
-                             self.SPECS_COL: json.loads(row[self.SPECS_COL]),
-                             self.RESPONSE_COL: json.loads(row[self.RESPONSE_COL])})
+        # convert CLEAR llm to ALTK llm
+        altk_llm_client = self.clear_llm_client_to_altk_llm_client(llm, config.get("provider"), config.get("eval_model_name"))
 
-        # call spark with pipeline over examples, results store sorted results over the examples
-        results = self.generate_spark_evaluation_results(examples,
-                                                         provider = config.get("provider"),
-                                                         model_name = config.get("eval_model_name"))
+        # call sparc with pipeline over examples, results store sorted results over the examples
+        results = self.generate_sparc_evaluation_results(df=df, llm_client=altk_llm_client)
 
         # extract output score and evaluation text from each results (concatenate failing explanations? minimum/average score over metrics?)
         for i, result in enumerate(results):
@@ -49,6 +48,47 @@ class ToolCallEvalUseCase(EvalUseCase):
         # Convert score column to nullable float type
         df[score_col] = df[score_col].astype('Float64')
         return df
+    
+
+    def clear_llm_client_to_altk_llm_client(self, llm, provider: str, model_name: str) -> BaseLLMClient:
+        """Convert CLEAR's LLM object to ALTK's LLM Object."""
+
+        if provider == "watsonx":
+            MetricsClientCls = get_llm("watsonx.output_val")
+            if llm.space_id:
+                llm_client = MetricsClientCls(
+                    model_id=llm.model_id,
+                    api_key=llm.api_key._secret_value,
+                    url=llm.url,
+                    space_id=llm.space_id,
+                )
+            elif llm.project_id:
+                llm_client = MetricsClientCls(
+                    model_id=llm.model_id,
+                    api_key=llm.api_key._secret_value,
+                    url=llm.url._secret_value,
+                    project_id=llm.project_id
+            )
+            else:
+                raise KeyError("Either space_id or project_id must be specified for watsonx inference.")
+            
+        elif provider == "azure":
+            MetricsClientCls = get_llm("azure_openai.async.output_val")
+            llm_client = MetricsClientCls(
+                model=model_name,
+                api_key=llm.api_key._secret_value,
+            )
+        elif provider == "openai":
+            MetricsClientCls = get_llm("openai.async.output_val")
+            llm_client = MetricsClientCls(
+                model=llm.model,
+            )
+        
+            llm_client = MetricsClientCls(model_name=model_name, api_key="")
+        else:
+            raise ValueError(f"Unsupported provider: {provider}")
+        
+        return llm_client
 
 
     @staticmethod
@@ -59,25 +99,50 @@ class ToolCallEvalUseCase(EvalUseCase):
     def get_default_generation_model_inputs(row, config):
         raise NotImplementedError("Tool Call generations must be provided")
 
-    def get_eval_from_results(self, result: Any) -> Tuple[str, float]:
-        # TODO: IMPLEMENT
-        return "Correct tool call", 1
+    def get_eval_from_results(self, result: SPARCReflectionResult) -> Tuple[str, float]:
+        """
+        Compute grade from LLMEvalKit pipeline result according to the specified logic.
 
-    def generate_spark_evaluation_results(self, examples: List[Dict[str, Any]], provider: str, model_name: str):
-        """Generates spark evaluation results."""
-        # examples - List[Dict[str, Any]], keys: context, api_spec, response (the tool call), values: the json objects
-        # provider - rits/watsonx/openai
+        Args:
+            result: PipelineResult from LLMEvalKit
 
-        # TODO
-        # spark_llm = get_spark_llm(provider, model_name)
-        # construct input examples from df (input fields spec_cols and context_col given)
-        # construct spark pipeline
-        return [None] * len(examples)
+        Returns:
+            Grade as float in [0, 1]
+        """
+        logger.debug("=== DEBUG: Full result structure ===")
+        logger.debug(result)
+        if result.decision.name == "APPROVE":
+            return "Tool call is valid.", 1.0
+        else:
+            explanation_text = "\n".join([issue.explanation for issue in result.issues])
+            logger.debug("=== DEBUG: Explanation Text ===")
+            logger.debug(explanation_text)
+            return f"Tool call is invalid. Reasons:\n{explanation_text}", 0.0
+    
+
+    def generate_sparc_evaluation_results(self, df: pd.DataFrame, llm_client: BaseLLMClient) -> List[SPARCReflectionResult]:
+        """Generates sparc evaluation results."""
+        sparc_component = SPARCReflectionComponent(
+            config=ComponentConfig(llm_client=llm_client),
+            track=Track.SLOW_TRACK,  # Use slow track for performance
+            execution_mode=SPARCExecutionMode.ASYNC,
+        )
+        reflection_results = []
+        for _, example in df.iterrows():
+            run_input = SPARCReflectionRunInput(
+                    messages=json.loads(example[self.CONTEXT_COL]),
+                    tool_specs=json.loads(example[self.SPECS_COL]),
+                    tool_calls=[json.loads(example[self.RESPONSE_COL])],
+                )
+            reflection_result = sparc_component.process(run_input, phase=AgentPhase.RUNTIME).output.reflection_result
+            reflection_results.append(reflection_result)
+        
+        return reflection_results
 
 
 if __name__ == "__main__":
-    DEFAULT_CONFIG_PATH = os.path.join("..", "setup", "default_config.yaml")
-    provider = "rits"  # watsonx, openai
+    DEFAULT_CONFIG_PATH = str(files("clear_eval.pipeline.setup").joinpath("default_config.yaml"))
+    provider = "watsonx"
     model_name = "meta-llama/llama-3-3-70b-instruct"
     config = load_config(DEFAULT_CONFIG_PATH, user_config_path=None, provider=provider, eval_model_name=model_name)
 
