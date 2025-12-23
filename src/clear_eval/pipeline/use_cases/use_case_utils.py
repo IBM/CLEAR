d3caafd1f0bbca0171b17ffec67172a0040144ce
lilacheden <lilach.edel@gmail.com>
2025-12-23 20:32:21 +0200

    don't import ToolCallEvalUseCase by default

--- src/clear_eval/pipeline/use_cases/use_case_utils.py
+++ src/clear_eval/pipeline/use_cases/use_case_utils.py
@@ -1,22 +1,14 @@
 from clear_eval.pipeline.use_cases.EvalUseCase import MathUseCase, RAGUseCase, GeneralEvalUseCase
-from clear_eval.pipeline.use_cases.ToolCallUseCase import ToolCallEvalUseCase
 
-task_to_use_case_class = {
-    "math": MathUseCase,
-    "rag": RAGUseCase,
-    "general": GeneralEvalUseCase,
-    "tool_call": ToolCallEvalUseCase
-}
+def get_task_data_obj(use_case_name):
+    if use_case_name == "math":
+        return MathUseCase()
+    elif use_case_name == "rag":
+        return RAGUseCase()
+    elif use_case_name == "general":
+        return GeneralEvalUseCase()
+    elif use_case_name == "tool_call":
+        from clear_eval.pipeline.use_cases.ToolCallUseCase import ToolCallEvalUseCase
+        return ToolCallEvalUseCase()
+    raise ValueError(f"Unsupported use case: {use_case_name}")
 
-def get_use_case_class(use_case_name):
-    try:
-        return task_to_use_case_class[use_case_name]
-    except KeyError:
-        raise ValueError(f"Unsupported use case: {use_case_name}")
-
-
-def get_task_data_obj(task):
-    return get_use_case_class(task)()
-
-def get_supported_use_case_classes():
-    return list(task_to_use_case_class.keys())
